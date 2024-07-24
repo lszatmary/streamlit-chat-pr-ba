259bc1f59c7362a9884c7f09fa0ec86feb891333
lszatmary <lszatmary78@gmail.com>
2024-07-24 22:53:30 +0200

    Connection to AWS S3. A button to erase streamlit session_state

--- session_state_json_interop.py
+++ session_state_json_interop.py
@@ -2,45 +2,51 @@ import json
 from pathlib import Path
 
 import streamlit as st
-from st_files_connection import FilesConnection
+import boto3
 
-filepath = Path("history_proba.json")
+def erase_messages():
+    st.session_state.messages = []
+    upload_file_to_s3()
 
-conn = st.connection('s3', type=FilesConnection)
-df = conn.read("lsz-streamlit-chat-proba-bucket-1/chat_proba_1/history_proba.json", input_format="json", ttl=600)
+def upload_file_to_s3():
+    s3.put_object(Body=json.dumps(st.session_state.messages).encode('UTF-8'),   #, encoding="utf-8"
+                  Bucket=bucket_name,
+                  Key=bucket_key,
+                  ContentType='application/json'
+                  )
+
+
+file_name = "history_proba.json"
+bucket_name = "lsz-streamlit-chat-proba-bucket-1"
+bucket_key = "/".join(("chat_proba_1", file_name))
+filepath = Path(file_name)
+
+s3 = boto3.client(
+        service_name='s3',
+        region_name=st.secrets.AWS_DEFAULT_REGION,
+        aws_access_key_id=st.secrets.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=st.secrets.AWS_SECRET_ACCESS_KEY,
+    )
+
+st.title("Json írás-olvasás s3-on")
+
+st.button("Session state törlése", on_click=erase_messages)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    with filepath.open("r", encoding="utf-8") as f:
-        history = json.load(f)
-    # print(type(history))
-    for m in history:
+    response = s3.get_object(Bucket=bucket_name, Key=bucket_key)
+    messages = json.loads(response["Body"].read().decode("utf-8"))
+    st.write(messages)
+    for m in messages:
         if isinstance(m, dict):
             st.session_state.messages.append(m)
 
-# st.write(st.session_state)
-st.write(st.secrets.PROBA)
-st.write(df)
-
-# TODO githubra felrakni és deployolni
-#TODO kiolvasás megy az s3-ról, de hogy tudok oda írni?
-# Talán ez segít:  https://discuss.streamlit.io/t/cannot-upload-streamlit-uploaded-file-to-s3-bucket/33376
-
-
-
-
-
-st.title("Json együttműködés")
-
-
-
-
 if user_input := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content":user_input})
-
     st.session_state.messages.append({"role": "assistant", "content":str("".join([c for c in user_input[::2]]))})
-    with filepath.open("w", encoding="utf-8") as f:
-        json.dump(st.session_state.messages, f)
+
+    upload_file_to_s3()
+
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
